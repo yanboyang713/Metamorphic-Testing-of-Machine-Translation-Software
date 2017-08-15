2972d4b4502c357c06aa8ffe9313c3ee0a740680
Boyang Yan <yanboyang713@Boyangs-MacBook-Air.local>
2017-08-16 00:38:23 +1000

    youdao api init

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from youdao import Youdao
+
+#main function
+def main():
+    youdao = Youdao()
+    #youdao.translate("EN", "zh_CHS", "this is an apple")
+if __name__ == "__main__":
+  main()
---
+++ youdao.py
@@ -0,0 +1,19 @@
+import random
+import sys
+import requests
+import hashlib
+class Youdao(object):
+    def __init__(self):
+        # app key
+        self.appKey = "0b2326799bc74b6e"
+        #pass ward
+        self.pwd = "h8YekUjoOWwK15BhlpsXJ0bxkgXv4XQE"
+
+    def translate(self, fromLanguage, toLanguage, translateText):
+        salt = random.randrange(10)
+        signString = self.appKey + translateText + str(salt) + self.pwd
+        sign = hashlib.md5(signString.encode())
+        url = "http://openapi.youdao.com/api?q=" + translateText + "&from=" + fromLanguage + "&to=" + toLanguage + "&appKey=" + self.appKey +  "&salt=" + str(salt) + "&sign=" + sign.hexdigest()
+        receiveMsg = requests.post(url)
+
+        print (receiveMsg.text)
