46ab71a66893060ba300b4fe275a886e3c15a801
yanboyang713 <yanboyang713@gmail.com>
2017-10-09 09:43:05 +1100

    update

--- main.py
+++ main.py
@@ -7,15 +7,10 @@ import time
 #main function
 def main():
     #Test obtain paragraph & sentence
-<<<<<<< HEAD
-    testData=Get_Literal()
-    testData.Get_Wordlist()
-=======
     l1=Get_Literal()
     l1.setTestData()
     print ("finish set up data")
     l1.print()
->>>>>>> origin/master
     #l1.Capture_Word()
     #P1=l1.Get_Paragraph()
     #print(P1)
@@ -24,15 +19,6 @@ def main():
     #print(S1)
 
     # Perform translations with a common iunterface
-<<<<<<< HEAD
-    #translator = CommonTranslator()
-    #print( translator.translate('Google', 'Hello, World!', 'fr', 'EN') )
-    #print( translator.translate('Bing', 'Hello, World!', 'fr', 'EN') )
-    #print( translator.translate('Youdao', 'Hello, World!', 'fr', 'EN') )
-    #print( translator.translate('All', 'Hello, World!', 'es', 'EN') )
-    #print('Complete')
-
-=======
     '''
     translator = CommonTranslator()
     print( translator.translate('Google', 'Hello, World!', 'fr', 'EN') )
@@ -41,7 +27,6 @@ def main():
     print( translator.translate('All', 'Hello, World!', 'es', 'EN') )
     print('Complete')
 '''
->>>>>>> origin/master
     # nltk
     #ownnltk = Nltk()
     #ownnltk.checkScore()
