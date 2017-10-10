35cd7ca52b3a45a5f16365766a867144d0ac061a
yanboyang713 <yanboyang713@gmail.com>
2017-10-10 15:37:35 +1100

    change bing key

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import sys
-from nltk import Nltk
+#from nltk import Nltk
 from commonTranslator import CommonTranslator
-from GetLiteral import Get_Literal
+#from GetLiteral import Get_Literal
 import time
 
 #main function
 def main():
     #Test obtain paragraph & sentence
-    setTestData = Get_Literal()
+    #setTestData = Get_Literal()
     #l1=Get_Literal()
     #l1.setTestData()
     #print ("finish set up data")
@@ -19,6 +19,9 @@ def main():
     #S1=l1.Get_Sentence()
     #print(S1)
 
+    translator = CommonTranslator()
+
+    print( translator.translate('Bing', 'Hello, World!', 'fr', 'EN') )
     # Perform translations with a common iunterface
 '''
 
