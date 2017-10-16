05df769cf31518ce1786ffc52be68c64b221683b
Robin Wang <31064412+Gravtt@users.noreply.github.com>
2017-10-16 17:39:26 +1100

    Merge separate running result(excel file) together

--- mergeFile.py
+++ mergeFile.py
@@ -1,31 +1,26 @@
 import openpyxl
 
+
 def main():
+    # used to merge a excel file: "read_file_2" into another excel file: "read_file_1"
     read_file_1=openpyxl.load_workbook(filename='translations.xlsx')
     file1_google_sheet=read_file_1['Google']
     file1_bing_sheet=read_file_1['Bing']
     file1_youdao_sheet=read_file_1['Youdao']
-    #print("11111")
+
+
     read_file_2=openpyxl.load_workbook(filename='finial.xlsx')
     file2_google_sheet=read_file_2['Google']
     file2_bing_sheet=read_file_2['Bing']
     file2_youdao_sheet=read_file_2['Youdao']
 
-    print(file2_google_sheet['A1'].value)
-    #print(file2_youdao_sheet.cell(row=1,column=9).value)
-
-    #print("22222")
-
-    #read_file_2=openpyxl.workbook()
-    #read_file_1=read_file_2.active
-
-    #print("111111")
-    #count=0
 
     #merge google translation
     askMergeGoogleSheet = input("Do you want to merge google sheet? (Y/n) : ")
     if askMergeGoogleSheet=='Y':
+        #set the the range by entering the sentence number for start and end.
         for i in range(1, 101):
+            #for each same row, if there is no sentence in read_file_1 but exist sentences in read_file_2, then merge that into file_1
             if file1_google_sheet.cell(row=i,column=1).value == None:
                 if file2_google_sheet.cell(row=i,column=1).value != None:
                         for j in range(1,10):
@@ -35,7 +30,9 @@ def main():
     # merge bing translation
     askMergeBingSheet = input("Do you want to merge bing sheet? (Y/n) : ")
     if askMergeBingSheet=='Y':
+        # set the the range by entering the sentence number for start and end.
         for i in range(1, 101):
+            # for each same row, if there is no sentence in read_file_1 but exist sentences in read_file_2, then merge that into file_1
             if file1_bing_sheet.cell(row=i,column=1).value == None:
                 if file2_bing_sheet.cell(row=i,column=1).value != None:
                         for j in range(1,10):
@@ -44,7 +41,9 @@ def main():
     # merge youdao translation
     askMergeYoudaoSheet = input("Do you want to merge youdao sheet? (Y/n) : ")
     if askMergeYoudaoSheet=='Y':
+        # set the the range by entering the sentence number for start and end.
         for i in range(1, 101):
+            # for each same row, if there is no sentence in read_file_1 but exist sentences in read_file_2, then merge that into file_1
             if file1_youdao_sheet.cell(row=i,column=1).value == None:
                 if file2_youdao_sheet.cell(row=i,column=1).value != None:
                         for j in range(1,10):
